623f3b9bbff397812696a5fe4e0f9f6088b954b1
Raniere Lucas <rlsmont123@hotmail.com>
2021-11-19 11:09:16 -0300

    FEATURE: adicionado os graus do IMC

--- js/imcCalc.js
+++ js/imcCalc.js
@@ -3,10 +3,8 @@ export const start = () => {
 
 	calculaImc(filtraInputs(data));
 
-    return data
-
+	return data;
 };
-
 
 const filtraInputs = (data) => {
 	if (peso.value !== "" && altura.value !== "") {
@@ -19,7 +17,31 @@ const filtraInputs = (data) => {
 	throw new Error("1");
 };
 const calculaImc = (data) => {
-    data.imc = data.p / data.a ** 2
-    
-    return data
-} 
+	data.imc = data.p / data.a ** 2;
+	data.check = true;
+	checaGrauImc(data);
+	return data;
+};
+
+const checaGrauImc = (data) => {
+	switch (data.check) {
+		case data.imc <= 18.5:
+			data.grau = "abaixo do peso";
+			break;
+		case data.imc <= 24.9:
+			data.grau = "com o peso normal";
+			break;
+		case data.imc <= 29.9:
+			data.grau = "com sobrepeso";
+			break;
+		case data.imc <= 34.9:
+			data.grau = "com obesidade grau I";
+			break;
+		case data.imc <= 39.9:
+			data.grau = "com obesidade grau II";
+			break;
+		default:
+			data.grau = "com obesidade grau III ou Mórbida";
+			break;
+	}
+};
--- js/render.js
+++ js/render.js
@@ -4,5 +4,7 @@ const altura = document.getElementById("altura");
 const resposta = document.getElementById("resposta");
 
 export const render = (e) => {
-	resposta.innerHTML = `<h2>Seu IMC é de ${e.imc.toFixed(1)}`;
+	resposta.innerHTML = `<h2>Seu IMC é de ${e.imc.toFixed(1)}, você está ${
+		e.grau
+	}`;
 };
